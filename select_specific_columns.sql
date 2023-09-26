c553208b97b0d45c8d7415d38f22857e52bf9003
Defne Buse Çelik <110555559+defnebusecelik@users.noreply.github.com>
2023-09-26 19:41:41 +0300

    Add files via upload

---
+++ select_specific_columns.sql
@@ -0,0 +1,13 @@
+-- select all data
+
+SELECT * FROM movies;
+
+SELECT * FROM actors;
+
+-- select specific columns
+
+SELECT first_name FROM actors;
+
+SELECT first_name, last_name FROM actors;
+
+SELECT movie_name, movie_lang FROM movies;
