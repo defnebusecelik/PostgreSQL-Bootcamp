2addaca35539e51a253c2f896ce905b940ce006b
Defne Buse Çelik <110555559+defnebusecelik@users.noreply.github.com>
2023-09-26 16:13:14 +0300

    Add files via upload

---
+++ Module 4/insert_multiple_records.sql
@@ -0,0 +1,9 @@
+-- insert multiple records into a table
+
+INSERT INTO customers (first_name,last_name)
+VALUES 
+('Linda','Adams'),
+('Taylor','Swift'),
+('Selena','Gomez');
+
+SELECT * FROM customers;
