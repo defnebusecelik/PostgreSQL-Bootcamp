d92ecaf526af3cd3e53b7d0156fc6c617ea8de5c
Defne Buse Çelik <110555559+defnebusecelik@users.noreply.github.com>
2023-12-08 11:50:02 +0300

    Add files via upload

---
+++ Module 11/create_a_postal_code_validation.sql
@@ -0,0 +1,14 @@
+-- create a postal code validation data type
+
+CREATE DOMAIN us_postal_code AS TEXT
+CHECK (VALUE ~ '^\d{5}$' OR VALUE ~'^\D{5}-\d{4}$');
+
+CREATE TABLE addresses(
+	address_id SERIAL PRIMARY KEY,
+	postal_code us_postal_code
+);
+
+INSERT INTO addresses (postal_code)
+VALUES (10000);
+
+SELECT * FROM addresses;
